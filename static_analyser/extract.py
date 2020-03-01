f1e7ce3c9263d0e02c8ea2bdba00d5945d8e7564
Nikhil Sasi Rajan <nikhil.rajan@essteyr.com>
2020-03-02 01:15:37 +0530

    broke preprocess into separate files

---
+++ static_analyser/extract.py
@@ -0,0 +1,5 @@
+from utility import read1, extract_word, isalpha
+
+def extract_cpp(in_filename:str):
+    ''' Function to extract list of functions in a class
+        and the position of their definition if found. '''
--- static_analyser/run.py
+++ static_analyser/run.py
@@ -5,12 +5,12 @@ testfiles = [
     '../testfiles/Controller.cpp',
 ]
 
-pp.strip_stuff(in_filepath=testfiles[0],
-               out_filepath='../__temp__.cpp',
-               single_line_comments=True,
-               multiline_comments=True,
-               strings=True,
-               ppd_includes=True,
-               ppd_defines=True,
-               skip_newline=True,
-               qt_macros=True)
+pp.strip_cpp(in_filepath=testfiles[0],
+             out_filepath='../__temp__.cpp',
+             single_line_comments=True,
+             multiline_comments=True,
+             strings=True,
+             ppd_includes=True,
+             ppd_defines=True,
+             skip_newline=True,
+             qt_macros=True)
